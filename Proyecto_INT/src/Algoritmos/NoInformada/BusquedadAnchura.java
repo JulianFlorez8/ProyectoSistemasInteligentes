1f20259a57a615ccd822203d0a26445132cbca87
Julian F <julian.1701913963@ucaldas.edu.co>
2023-04-16 15:51:27 -0500

    camino anchura

--- Proyecto_INT/src/Algoritmos/NoInformada/BusquedadAnchura.java
+++ Proyecto_INT/src/Algoritmos/NoInformada/BusquedadAnchura.java
@@ -6,8 +6,11 @@
 package Algoritmos.NoInformada;
 
 import Modelos.Nodo;
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.Map;
 import java.util.Queue;
+import java.util.Stack;
 
 /**
  *
@@ -15,17 +18,36 @@ import java.util.Queue;
  */
 public class BusquedadAnchura {
 
-    public  void bfs(Nodo root, String buscado) {
+    public void bfs(Nodo root, String buscado) {
         System.out.println(root);
         Queue<Nodo> queue = new LinkedList<Nodo>();
         root.setVisitado(true);
         queue.add(root);
+
+        Map<Nodo, Nodo> padres = new HashMap<Nodo, Nodo>(); // diccionario de padres, hijo clave, padre valor
+
         while (!queue.isEmpty()) {
             Nodo nodoActual = queue.remove();
-            System.out.println(nodoActual);
-            if(nodoActual.getNombre().equals(buscado))
-            {
-                System.out.println("encontrado");
+            //System.out.println(nodoActual); //para imprimir los nodos que se van expandiendo
+
+            if(nodoActual.getNombre().equals(buscado)) {
+                //System.out.println("encontrado");
+
+                // seguimos el camino de padres desde el nodo buscado hasta la raíz
+                Nodo nodo = nodoActual;
+                Stack<Nodo> camino = new Stack<Nodo>(); // pila de nodos en el camino
+                camino.push(nodo);
+                while (nodo != root) {
+                    nodo = padres.get(nodo);
+                    camino.push(nodo);
+                }
+
+                // desapilamos los nodos de la pila para imprimirlos en orden
+                System.out.print(camino.pop().getNombre());
+                while (!camino.isEmpty()) {
+                    System.out.print(" -> " + camino.pop().getNombre());
+                }
+
                 break;
             }
 
@@ -33,6 +55,9 @@ public class BusquedadAnchura {
                 if (!nodo.isVisitado()) {
                     nodo.setVisitado(true);
                     queue.add(nodo);
+
+                    // guardamos el padre del nodo recién agregado en el diccionario
+                    padres.put(nodo, nodoActual);
                 }
             }
         }
